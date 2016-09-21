cb90194cdd0826676260fac35d61ccbdf2106941
ArunNivethan <ruppaarun94@gmail.com>
2016-09-21 16:56:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article_one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
 });
 app.get('/article_two', function (req, res) {
-  res.send('Article two request and will be server ');
+   res.sendFile(path.join(__dirname, 'ui', 'article_two.html'));
 });
 app.get('/article_three', function (req, res) {
-  res.send('Article three request and will be server ');
+   res.sendFile(path.join(__dirname, 'ui', 'article_three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
