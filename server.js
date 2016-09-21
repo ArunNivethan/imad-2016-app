e60df9b11d919790fac404cf8f55d333c919ec87
ArunNivethan <ruppaarun94@gmail.com>
2016-09-21 15:22:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article_one', function (req, res) {
+  res.sendFile('Article one request and will be server ');
+});
+app.get('/article_two', function (req, res) {
+  res.sendFile('Article one request and will be server ');
+});
+app.get('/article_three', function (req, res) {
+  res.sendFile('Article one request and will be server ');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
