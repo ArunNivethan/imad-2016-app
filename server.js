a060a1e9845b92bc1b1e3385a618b9f72711c866
ArunNivethan <ruppaarun94@gmail.com>
2016-09-22 16:35:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,32 @@ var articleOne={
             </ol>`
     
 };
-
+var article_temp=`<html>
+    <head>
+        <title>
+           ${title}
+        </title>
+        <meta name="viewport" content="width-device-width ,initial-scale-1"/>
+         <link href="/ui/style.css" rel="stylesheet" />
+    
+    </head>
+    <boby>
+        <div class="container">
+        <div>
+            <a href ="/">Home</a>
+        </div>
+        <hr/>
+        <h3>${heading} </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+           ${content}
+            </div>
+            </div>
+    </boby>
+</html>
+`;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
