0265d7a8d572dccdb345114d6ffcf433c49987c3
ArunNivethan <ruppaarun94@gmail.com>
2016-09-22 20:18:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,13 @@ var element = document.getElementById('main-text');
 element.innerHTML="arun";
 //going to move image
 var img=document.getElementById('ar');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft +5;
+    img.style.marginLeft=marginLeft+'px';
+}
  img.onclick=function()
 {
- img.style.marginLeft ='100px';   
+ var interval=setInterval(moveRight, 50);  
 };
