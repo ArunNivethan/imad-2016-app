488652c50c57f8d15f2049dad32b514753ea1da9
ArunNivethan <ruppaarun94@gmail.com>
2016-09-26 20:42:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,19 @@ var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function()
 {
-    counter=counter+1;
-var span=document.getElementById('count');
-span.innerHTML=counter.toString();
+    var request=new XMLHttprequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readystate===XMLHttprequest.DONE)
+        {
+            if (request.status===200)
+            {
+                var counter=request.responseTest;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET',arunnivethan.imad.hasura-app.io/counter,true);
+   request.send(null);
 };
