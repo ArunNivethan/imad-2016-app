11f9b0117e06540dcebc43448c6d9114d8f79f50
ArunNivethan <ruppaarun94@gmail.com>
2016-09-26 20:13:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,9 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML="arun";
-//going to move image
-var img=document.getElementById('ar');
-var marginLeft=0;
-function moveRight()
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function()
 {
-    marginLeft=marginLeft +5;
-    img.style.marginLeft=marginLeft+'px';
-}
- img.onclick=function()
-{
- var interval=setInterval(moveRight, 50);  
+    counter=counter+1;
+    
 };
+var span=document.getElementById('count');
+span.innerHTMl=counter.tostring();
