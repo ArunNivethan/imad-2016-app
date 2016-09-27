94d15c8d5c8ffcd98b4beef93f5b6cd409ed04bb
ArunNivethan <ruppaarun94@gmail.com>
2016-09-27 17:43:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,9 +23,9 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_id');
 var names=['name1','name2','name3','name4'];
 var list='';
-for(var i=0;i<name.length;i++)
+for(var i=0;i<names.length;i++)
 {
-    list +='<li>'+ name[i]+'</li>';
+    list +='<li>'+ names[i]+'</li>';
     
 }
 var ul=document.getElementById('namelist');
